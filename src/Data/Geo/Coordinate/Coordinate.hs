2cf928dc9fd655215556be784a9c98150d683045
Tony Morris <tmorris@tmorris.net>
2014-09-23 14:57:54 +1000

    more Prisms, fix more imports

--- src/Data/Geo/Coordinate/Coordinate.hs
+++ src/Data/Geo/Coordinate/Coordinate.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE RankNTypes #-}
 
 module Data.Geo.Coordinate.Coordinate(
   Coordinate
 , (.#.)
-, (..#..)
+, (<°>)
+, (<㎭>)
+, fracCoordinate
+, radianCoordinate
 , HasCoordinate(..)
 , coordinateLatLon
 , coordinateLonLat
@@ -13,17 +17,18 @@ module Data.Geo.Coordinate.Coordinate(
 ) where
 
 import Control.Category(Category(id, (.)))
-import Control.Lens(Iso', Lens', iso, lens, mapping, swapped, withIso, (^?))
+import Control.Lens(Iso', Lens', Prism', iso, lens, prism', mapping, swapped, withIso, (^?), (#))
 import Control.Monad(Monad(return))
 import Data.Eq(Eq)
-import Data.Geo.Coordinate.Latitude
-import Data.Geo.Coordinate.Longitude
-import Data.Geo.Coordinate.DegreesLatitude
-import Data.Geo.Coordinate.DegreesLongitude
-import Data.Geo.Coordinate.Minutes
-import Data.Geo.Coordinate.Seconds
+import Data.Geo.Coordinate.Latitude(HasLatitude(latitude), Latitude, dmsLatitude, radianLatitude, fracLatitude)
+import Data.Geo.Coordinate.Longitude(HasLongitude(longitude), Longitude, dmsLongitude, radianLongitude, fracLongitude)
+import Data.Geo.Coordinate.DegreesLatitude(HasDegreesLatitude(degreesLatitude), DegreesLatitude)
+import Data.Geo.Coordinate.DegreesLongitude(HasDegreesLongitude(degreesLongitude), DegreesLongitude)
+import Data.Geo.Coordinate.Minutes(Minutes)
+import Data.Geo.Coordinate.Seconds(Seconds)
 import Data.Maybe(Maybe)
 import Data.Ord(Ord)
+import Data.Tuple(curry)
 import Prelude(Show, Double)
 
 data Coordinate =
@@ -42,15 +47,55 @@ data Coordinate =
 
 -- | Build a coordinate from a fractional latitude and fractional longitude. Fails
 -- if either are out of range.
-(..#..) ::
+(<°>) ::
   Double
   -> Double
   -> Maybe Coordinate
-lat ..#.. lon =
-  do lat' <- lat ^? fracLatitude
-     lon' <- lon ^? fracLongitude
-     return (lat' .#. lon')
+(<°>) =
+  curry (^? fracCoordinate)
 
+-- | Build a coordinate from a radian latitude and fractional longitude. Fails
+-- if either are out of range.
+(<㎭>) ::
+  Double
+  -> Double
+  -> Maybe Coordinate
+(<㎭>) =
+  curry (^? radianCoordinate)
+ 
+coordinatePrism ::
+  Prism' Double Latitude
+  -> Prism' Double Longitude
+  -> Prism' (Double, Double) Coordinate
+coordinatePrism f g =
+  prism'
+    (\x -> let (b, d) = coordinateLatLon # x
+           in (f # b, g # d))
+    (\(lat, lon) ->
+      do lat' <- lat ^? f
+         lon' <- lon ^? g
+         return (Coordinate lat' lon'))
+
+-- | A prism on the pair (below) to a coordinate:
+-- 
+-- * a fractional latitude to a double between -90 and 90 exclusive.
+--
+-- * a fractional longitude to a double between -180 and 180 exclusive.
+fracCoordinate ::
+  Prism' (Double, Double) Coordinate
+fracCoordinate =
+  coordinatePrism fracLatitude fracLongitude
+
+-- | A prism on the pair (below) to a coordinate:
+-- 
+-- * a radian latitude to a double between -π/2 and π/2 exclusive.
+--
+-- * a radian longitude to a double between -π and π exclusive.
+radianCoordinate ::
+  Prism' (Double, Double) Coordinate
+radianCoordinate =
+  coordinatePrism radianLatitude radianLongitude
+  
 coordinateLatLon ::
   Iso' (Latitude, Longitude) Coordinate
 coordinateLatLon =
