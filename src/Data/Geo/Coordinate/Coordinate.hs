bc9a18fd99e74e1eb8bbf86e9b040149c2a7b9d4
Tony Morris <tmorris@tmorris.net>
2015-07-29 12:53:32 +1000

    cooordinate 0.0.19

--- src/Data/Geo/Coordinate/Coordinate.hs
+++ src/Data/Geo/Coordinate/Coordinate.hs
@@ -16,12 +16,12 @@ module Data.Geo.Coordinate.Coordinate(
 , (<°>)
 ) where
 
-import Control.Applicative(Applicative)
+import Control.Applicative(Applicative((<*>)))
 import Control.Category(Category(id, (.)))
-import Control.Lens(Identity, Const, Prism', Lens', Choice, swapped, Profunctor, Optic', (^.), iso, lens, prism', swapped, (^?), (#))
+import Control.Lens(Identity, Const, Prism', Choice, swapped, Profunctor, Optic', (^.), iso, from, lens, prism', swapped, (^?), (#))
 import Control.Monad(Monad(return))
 import Data.Eq(Eq)
-import Data.Functor(Functor)
+import Data.Functor(Functor, (<$>))
 import Data.Geo.Coordinate.Latitude(AsLatitude(_Latitude), Latitude)
 import Data.Geo.Coordinate.Longitude(AsLongitude(_Longitude), Longitude)
 import Data.Geo.Coordinate.DegreesLatitude(AsDegreesLatitude(_DegreesLatitude), DegreesLatitude)
@@ -43,11 +43,17 @@ data Coordinate =
 
 -- | Build a coordinate from a latitude and longitude.
 (.#.) ::
-  Latitude
-  -> Longitude
+  (  
+    AsLatitude (->) (Const Latitude) lat
+  , AsLongitude (->) (Const Longitude) lon
+  ) =>
+  lat
+  -> lon
   -> Coordinate
-(.#.) =
+lat .#. lon =
   Coordinate
+    (lat ^. _Latitude)
+    (lon ^. _Longitude)
 
 -- | Build a coordinate from a fractional latitude and fractional longitude. Fails
 -- if either are out of range.
@@ -164,30 +170,44 @@ instance (p ~ (->), Functor f) => AsDegreesLongitude p f Coordinate where
   _DegreesLongitude =
     _Longitude . _DegreesLongitude
 
-latitudeMinutes ::
-  Lens'
-    Coordinate
-    Minutes
+instance (p ~ (->), Applicative f) => AsMinutes p f Coordinate where
+  _Minutes f (Coordinate lat lon) =
+    (\m1 m2 ->
+      Coordinate
+        (from _Latitude # (lat ^. _DegreesLatitude, m1, lat ^. _Seconds))
+        (from _Longitude # (lon ^. _DegreesLongitude, m2, lon ^. _Seconds))) <$> 
+    f (lat ^. _Minutes) <*> 
+    f (lon ^. _Minutes)
+    
+instance (p ~ (->), Applicative f) => AsSeconds p f Coordinate where
+  _Seconds f (Coordinate lat lon) =
+    (\s1 s2 ->
+      Coordinate
+        (from _Latitude # (lat ^. _DegreesLatitude, lat ^. _Minutes, s1))
+        (from _Longitude # (lon ^. _DegreesLongitude, lon ^. _Minutes, s2))) <$> 
+    f (lat ^. _Seconds) <*> 
+    f (lon ^. _Seconds)
+
+latitudeMinutes ::  
+  (AsCoordinate (->) f coord, Functor f) =>
+  Optic' (->) f coord Minutes  
 latitudeMinutes =
-  _Latitude . _Minutes
+  _Coordinate . _Latitude . _Minutes
 
 latitudeSeconds ::
-  Lens'
-    Coordinate
-    Seconds
+  (AsCoordinate (->) f coord, Functor f) =>
+  Optic' (->) f coord Seconds
 latitudeSeconds =
-  _Latitude . _Seconds
+  _Coordinate . _Latitude . _Seconds
 
 longitudeMinutes ::
-  Lens'
-    Coordinate
-    Minutes
+  (AsCoordinate (->) f coord, Functor f) =>
+  Optic' (->) f coord Minutes  
 longitudeMinutes =
-  _Longitude . _Minutes
+  _Coordinate . _Longitude . _Minutes
 
 longitudeSeconds ::
-  Lens'
-    Coordinate
-    Seconds
+  (AsCoordinate (->) f coord, Functor f) =>
+  Optic' (->) f coord Seconds
 longitudeSeconds =
-  _Longitude . _Seconds
+  _Coordinate . _Longitude . _Seconds
