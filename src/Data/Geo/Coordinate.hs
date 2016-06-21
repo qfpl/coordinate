9898af385216b332882b4e667431ded13a0f40b4
Tony Morris <tmorris@tmorris.net>
2016-06-21 14:56:38 +1000

    ECEF

--- src/Data/Geo/Coordinate.hs
+++ src/Data/Geo/Coordinate.hs
@@ -9,7 +9,7 @@ module Data.Geo.Coordinate where
 import Control.Category((.))
 import Control.Applicative(Applicative((<*>), pure), liftA2, Alternative((<|>), empty))
 import Control.Category(id)
-import Control.Lens(makeClassy, Traversal',  Iso', Lens', lens, iso, from, (^.), (&), Wrapped(Unwrapped, _Wrapped'), Rewrapped)
+import Control.Lens(makeClassy, Traversal', Iso', Lens', lens, iso, from, (^.), (&), Wrapped(Unwrapped, _Wrapped'), Rewrapped)
 import Control.Monad(Monad((>>=), return), MonadPlus(mzero, mplus))
 import Control.Monad.Fix(MonadFix(mfix))
 import Control.Monad.IO.Class(MonadIO(liftIO))
@@ -22,18 +22,6 @@ import Data.Functor.Identity(Identity(Identity, runIdentity))
 import Data.Ord(Ord)
 import Prelude(Show, Double, Num((*), (+), (-)), Fractional((/)))
 
-data ECEF =
-  ECEF {
-    _x ::
-      Double
-  , _y ::
-      Double
-  , _z ::
-      Double
-  } deriving (Eq, Ord, Show)
-
-makeClassy ''ECEF
-
 class HasDoubles a where
   doubles ::
     Traversal' a Double
@@ -42,12 +30,47 @@ instance HasDoubles Double where
   doubles =
     id
 
-instance HasDoubles ECEF where
-  doubles f (ECEF x_ y_ z_) =
-    ECEF <$>
+data XY =
+  XY {
+    _x ::
+      Double
+  , _y ::
+      Double
+  }
+  deriving (Eq, Ord, Show)
+
+makeClassy ''XY
+
+instance HasDoubles XY where
+  doubles f (XY x_ y_) =
+    XY <$>
       f x_ <*>
-      f y_ <*>
+      f y_
+
+data ECEF =
+  ECEF {
+    _xy ::
+    XY
+  , _z ::
+      Double
+  }
+  deriving (Eq, Ord, Show)
+
+makeClassy ''ECEF
+
+instance HasXY ECEF where
+  xY =
+    lens
+      (\(ECEF xy_ _) -> xy_)
+      (\(ECEF _ z_) xy_ -> ECEF xy_ z_)
+
+instance HasDoubles ECEF where
+  doubles f (ECEF xy_ z_) =
+    ECEF <$>
+      doubles f xy_ <*>
       f z_
+
+----
 
 data Ellipsoid =
   Ellipsoid {
@@ -198,22 +221,127 @@ semiMinor ::
 semiMinor e =
   let Ellipsoid m f = e ^. ellipsoid
   in m * (1 - 1 / f)
-  
+
+eccentricitySquared ::
+  Applicative f =>
+  EllipsoidReaderT f Double
+eccentricitySquared =
+  (\f -> 2 * f - (f * f)) <$> readFlattening
+
+eccentricitySquared' ::
+  Applicative f =>
+  EllipsoidReaderT f Double
+eccentricitySquared' =
+  (\f -> (f * (2 - f)) / (1 - f * f)) <$> readFlattening
+
+----
+
+data LL =
+  LL {
+    _lat ::
+      Double -- radians
+  , _lon ::
+      Double -- radians
+  }
+  deriving (Eq, Ord, Show)
+
+makeClassy ''LL
+
+instance HasDoubles LL where
+  doubles f (LL a o) =
+    LL <$>
+      f a <*>
+      f o
+
 data LLH =
   LLH {
-    _lat ::
-      Double
-  , _lon ::
-      Double
+    ll ::
+      LL
   , _height ::
       Double
   } deriving (Eq, Ord, Show)
 
 makeClassy ''LLH
 
+instance HasLL LLH where
+  lL =
+    lens
+      ll
+      (\(LLH _ h) l -> LLH l h)
+
 instance HasDoubles LLH where
-  doubles f (LLH a o h) =
+  doubles f (LLH l h) =
     LLH <$>
-      f a <*>
-      f o <*>
+      doubles f l <*>
       f h
+
+----
+
+
+earthGeo ::
+  -- Applicative f =>
+  Iso' ECEF LLH
+earthGeo =
+  earthGeo
+
+{-
+
+
+> earthToGeo WGS84 (999 *~ metre, 88888 *~ metre, 7777 *~ metre)
+(0.16477260829264312,1.5595579375715796,-6288597.323607219 m)
+
+> earthToGeo WGS84 ((-5014967.33) *~ metre, 2614625.24 *~ metre, 2938968.021 *~ metre)
+(0.4820000002381146,2.6609999992939435,99.99797226581722 m)
+
+> geoToEarth (Geodetic (27.65 *~ degree) (152.45 *~ degree) (100 *~ metre) WGS84)
+(-5012801.572508958 m,2615061.7970748367 m,2942250.433250758 m)
+
+> geoToEarth (Geodetic (0.482 *~ radian) (2.661 *~ radian) (100 *~ metre) WGS84)
+(-5014967.3340606885 m,2614625.2376137706 m,2938968.02060036 m)
+
+
+geoToEarth :: (Ellipsoid e) => Geodetic e -> ECEF
+geoToEarth geo = (
+      (n + h) * coslat * coslong,
+      (n + h) * coslat * sinlong,
+      (n * (_1 - eccentricity2 e) + h) * sinlat)
+   where 
+      n = normal e $ latitude geo
+      e = ellipsoid geo
+      coslat = cos $ latitude geo
+      coslong = cos $ longitude geo
+      sinlat = sin $ latitude geo
+      sinlong = sin $ longitude geo
+      h = altitude geo
+
+normal :: (Ellipsoid e) => e -> Angle Double -> Length Double
+normal e lat = majorRadius e / sqrt (_1 - eccentricity2 e * sin lat ^ pos2)
+
+eccentricity2 :: (Ellipsoid e) => e -> Dimensionless Double
+eccentricity2 e = _2 * f - (f * f) where f = flattening e
+
+
+earthToGeo :: (Ellipsoid e) => e -> ECEF -> (Angle Double, Angle Double, Length Double)
+earthToGeo e (x,y,z) = (phi, atan2 y x, sqrt (l ^ pos2 + p2) - norm)
+   where
+      -- Naming: numeric suffix inicates power. Hence x2 = x * x, x3 = x2 * x, etc.
+      p2 = x ^ pos2 + y ^ pos2
+      a = majorRadius e
+      a2 = a ^ pos2
+      e2 = eccentricity2 e
+      e4 = e2 ^ pos2
+      zeta = (_1-e2) * (z ^ pos2 / a2)
+      rho = (p2 / a2 + zeta - e4) / _6
+      rho2 = rho ^ pos2
+      rho3 = rho * rho2
+      s = e4 * zeta * p2 / (_4 * a2)
+      t = cbrt (s + rho3 + sqrt (s * (s + _2 * rho3)))
+      u = rho + t + rho2 / t
+      v = sqrt (u ^ pos2 + e4 * zeta)
+      w = e2 * (u + v - zeta) / (_2 * v)
+      kappa = _1 + e2 * (sqrt (u + v + w ^ pos2) + w) / (u + v)
+      phi = atan (kappa * z / sqrt p2)
+      norm = normal e phi
+      l = z + e2 * norm * sin phi
+
+-}
