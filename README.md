20617006988f8668338aae82ad3bf20d77746cbe
George Wilson <george@wils.online>
2018-04-06 16:27:59 +1000

    Replace doctest with HUnit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Coordinate
 
+<img src="http://i.imgur.com/0h9dFhl.png" width="300px"/>
+
 Provides data types and libraries for manipulating values of latitude and longitude, typically used against a WGS84 reference ellipsoid. An emphasis is placed on utilising [lenses](http://hackage.haskell.org/package/lens/docs/Control-Lens-Lens.html), [prisms](http://hackage.haskell.org/package/lens/docs/Control-Lens-Prism.html) and [isomorphisms](http://hackage.haskell.org/package/lens/docs/Control-Lens-Iso.html) for manipulation of the structures.
 
--- src/Data/Geodetic/Sphere.hs
+++ src/Data/Geodetic/Sphere.hs
@@ -13,34 +13,17 @@ module Data.Geodetic.Sphere(
 import Data.Geodetic.Ellipsoid(Ellipsoid(Ellipsoid))
 import Papa
 
--- $setup
--- >>> import Control.Lens
-
 newtype Sphere =
   Sphere Double
   deriving (Eq, Ord, Show)
 
 makeWrapped ''Sphere
 
--- |
---
--- >>> earthMean
--- Sphere 6367450.0
 earthMean ::
   Sphere
 earthMean =
   Sphere 6367450
 
--- |
---
--- >>> ellipsoidSphere # Sphere 77
--- Ellipsoid {_semiMajor = 77.0, _flattening = 1.0}
---
--- >>> Ellipsoid 77 1 ^? ellipsoidSphere
--- Just (Sphere 77.0)
---
--- >>> Ellipsoid 77 2 ^? ellipsoidSphere
--- Nothing
 ellipsoidSphere ::
   Prism'
     Ellipsoid
